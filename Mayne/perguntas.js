5c605232d20f0f30c1a3b490557ccf06e253abb7
maynefermino <161840610+maynefermino@users.noreply.github.com>
2024-11-01 10:52:11 -0300

    Add files via upload

---
+++ Mayne/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Carro',
+    'Qual a marca de carro mais vendido do mundo ?',
+    'É da Toyota'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Italia?',
+    'A capital da Italia é a Roma'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua Italiana',
+    'Como se diz oi em Italino?',
+    'Oi em italiano é EHI'
+)
